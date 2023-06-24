aa9020482d9acd9f60bca10aa2d9a8d9fc27eca9
Artur <volk19011@gmail.com>
2023-06-25 01:59:41 +0300

    Fix 9 and 11 task

--- HW 6/Program.cs
+++ HW 6/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {;
+        {
             Console.WriteLine(string.Join(" ", Enumerable.Range(10, 41).ToArray()));
 
             Console.WriteLine(string.Join(" ", Enumerable.Range(10, 41).Where(n => n % 3 == 0).ToArray()));
@@ -20,11 +20,11 @@
 
             Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Average(s => s.Count()));
 
-            Console.WriteLine("aaa;xabbx;abb;ccc;dap;zh".Split(';').Reverse().Min(s => s.Count()));
+            Console.WriteLine(new string("aaa;xabbx;abb;ccc;dap;zh".Split(';').MinBy(s => s.Count()).Reverse().ToArray()));
 
             Console.WriteLine("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').Any(s => s.StartsWith("aa") && s.Skip(2).All(ss => ss == 'b')));
 
-            Console.WriteLine(new string("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').Where(s => s.EndsWith("bb")).Last().Skip(2).ToArray()));
+            Console.WriteLine(new string("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').Where(s => s.EndsWith("bb")).Skip(2).LastOrDefault()));
 
         }
     }
